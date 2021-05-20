fdcef68853763c957c2d413bd4bb716195b863e3
stevenverwer <stevenverwer@gmail.com>
2021-05-20 15:29:40 +0200

    for publish 0.0.1

--- setup.py
+++ setup.py
@@ -16,6 +16,14 @@ setup(
   keywords = ['PARALLEL POOLING', 'MULTI CORE', 'PDF', 'SCRAPER', 'PDFMINER', 'PDFMINER.SIX'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pdfminer.six',
+          'pathlib',
+          'os',
+          'tqdm',
+          'math',
+          'multiprocessing',
+          'csv',
+          're',
+          'io',          
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
